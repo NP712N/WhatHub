814494c466c4541513694d52ab3705015d23436d
NPN <npn19998@gmail.com>
2021-08-25 20:22:50 +0700

    create temp commit

--- Applications/WhatHubWebApp/src/app/home/home-page/content/post-list/post-list.component.ts
+++ Applications/WhatHubWebApp/src/app/home/home-page/content/post-list/post-list.component.ts
@@ -14,17 +14,49 @@ export class PostListComponent implements OnInit {
   posts: Post[];
 
   constructor(private _postService: PostService) {
+    this.posts = [
+      {
+        id: 1,
+        title: '1',
+        createDate: new Date(),
+        image: '',
+        isAnonymous: false,
+        lastModify: new Date(),
+        postByUserId: 1,
+        content: '1'
+      } as Post,
+      {
+        id: 2,
+        title: '2',
+        createDate: new Date(),
+        image: '',
+        isAnonymous: false,
+        lastModify: new Date(),
+        postByUserId: 1,
+        content: '1'
+      } as Post,
+      {
+        id: 3,
+        title: '3',
+        createDate: new Date(),
+        image: '',
+        isAnonymous: false,
+        lastModify: new Date(),
+        postByUserId: 1,
+        content: '1'
+      } as Post,
+    ];
   }
 
   ngOnInit(): void {
-    this.loading = true;
-    this._postService.getBriefPosts()
-      .pipe(finalize(() => {
-        this.loading = false;
-      }))
-      .subscribe(data => {
-        this.posts = data;
-      })
+    // this.loading = true;
+    // this._postService.getBriefPosts()
+    //   .pipe(finalize(() => {
+    //     this.loading = false;
+    //   }))
+    //   .subscribe(data => {
+    //     this.posts = data;
+    //   })
   }
 
 }
