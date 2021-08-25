cb24f2df877f0f5c2065af2d645e27e7767d1336
Duy Huynh <huynhtruongdyu@gmail.com>
2021-08-25 20:20:13 +0700

    wip

---
+++ Libraries/1.Helper/EnvironmentHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.Helper
+{
+    public static class EnvironmentHelper
+    {
+        public static string GetByKey(string key)
+        {
+            return Environment.GetEnvironmentVariable(key);
+        }
+    }
+}
--- Services/Portal/Portal.API/Controllers/WeatherForecastController.cs
+++ Services/Portal/Portal.API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using _1.Helper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -26,6 +27,7 @@ namespace Portal.API.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var a = EnvironmentHelper.GetByKey("ComSpec");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
